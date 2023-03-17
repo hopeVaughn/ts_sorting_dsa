a6dd4956b27577f36d71b166bcdf58a6db9e273f
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-17 09:18:15 -0700

    add array class and solution for sorter

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,6 @@
-class Sorter {
-  // this current iteration does not take into consideration characters or a linked list.
-  constructor (public collection: number[] | string) { }
-
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // if collection is an array of numbers
-        // all of this only works if collection is an array of numbers
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j]
-            this.collection[j] = this.collection[j + 1]
-            this.collection[j + 1] = leftHand
-          }
-        }
-        //only works if collection is a string
-        // if collection is a string 
-        if (typeof this.collection === 'string') {
-
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0])
+import { Sorter } from './Sorter'
+import { NumbersCollection } from './NumbersCollection'
+const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+const sorter = new Sorter(numbersCollection)
 sorter.sort()
-console.log(sorter.collection);
+console.log(numbersCollection.data);
