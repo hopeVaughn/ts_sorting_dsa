797f0e34a1312375409fcb1d97f38497a15c604f
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-17 09:45:17 -0700

    finish logic for charactersCollection class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
-const numbersCollection = new NumbersCollection([50, 3, -5, 0])
-const sorter = new Sorter(numbersCollection)
-sorter.sort()
-console.log(numbersCollection.data);
+import { CharactersCollection } from './CharactersCollection'
+// const numbersCollection = new NumbersCollection([50, 3, -5, 0])
+// const sorter = new Sorter(numbersCollection)
+// sorter.sort()
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaaybasldkfh')
+const sorter = new Sorter(charactersCollection)
+sorter.sort();
+console.log(charactersCollection.data);
